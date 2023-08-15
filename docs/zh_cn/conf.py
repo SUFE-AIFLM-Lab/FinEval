87bf1bc7a6ce695dc67fc248bf93c75fb0d1d01f
SUFE-AIFLM-Lab <45417254+SUFE-AIFLM-Lab@users.noreply.github.com>
2023-08-15 12:02:58 +0800

    Update conf.py

--- docs/zh_cn/conf.py
+++ docs/zh_cn/conf.py
@@ -84,7 +84,7 @@ html_theme_options = {
     'menu': [
         {
             'name': 'GitHub',
-            'url': 'https://github.com/SUFE-AIFLM/FinEval'
+            'url': 'https://github.com/SUFE-AIFLM-Lab/FinEval'
         },
     ],
     # Specify the language of shared menu
